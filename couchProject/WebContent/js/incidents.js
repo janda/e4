90449942240140904a3fc6f75a3e7890651601e9
janda <sjandamail-misc@yahoo.com>
2012-06-25 16:31:47 -0400

    Bind incident list to dynamic json from couchDb via jsp.

---
+++ couchProject/WebContent/js/incidents.js
@@ -0,0 +1,12 @@
+
+$( document ).delegate("#incidentListPage", "pageinit", function() {
+	  		
+	$.getJSON('../jsp/test.jsp', function(data) {				
+		$("#incidentListDiv").append('<ul data-role=\"listview\" data-inset=\"true\" data-filter=\"true\" id=\"incidentList\"></ul>');
+		$.each(data.rows, function(key, val) {
+			$("#incidentList").append('<li><a href=\"incidentForm.html\">' + val.key + '</a></li>');		
+		});		
+		$("#incidentListDiv").trigger('create');
+	});				  		
+	  		
+});	  		
---
+++ couchProject/WebContent/jsp/test.jsp
@@ -0,0 +1,3 @@
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
+
+<c:import url="http://localhost:5984/e4/_design/processing/_view/all_incidents" />
